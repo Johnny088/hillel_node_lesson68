1036fe01062e9dfd82775490f37507db14f24cc6
Johnny <espgoodlife22@gmail.com>
2026-06-21 17:40:06 +0300

    done

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
+const filePath = path.join('src', 'test.txt');
+// const filePath2 = path.join('src', 'test2.txt');
+
+const readableStream = fs.createReadStream(filePath, {
+  highWaterMark: 1024,
+});
+
+let counter = 1;
+let total = 0;
+
+readableStream.on('data', chunk => {
+  console.log(`Chunk #${counter} ${chunk.byteLength} bytes`);
+  counter += 1;
+  total += chunk.byteLength;
+});
+
+readableStream.on('end', () => {
+  console.log(`Total size: ${total} bytes`);
+});
